8d5479a3daf2830f29caac78e607b5fa7b41afc2
Trần Văn Dũng <148711058+trandung09@users.noreply.github.com>
2023-11-21 22:33:02 +0700

    Update QuickSortt.cpp

--- QuickSortt.cpp
+++ QuickSortt.cpp
@@ -9,13 +9,23 @@ using namespace std;
 template <class T>
 int partition(T A[], int L, int R){
 	T pivot = A[L];
-	int left = L, right = R;
-	while(1){
-		while(A[left] < pivot) left++;
-		while(A[right] > pivot) right--;
+	int left = L-1, right = R+1;
+	
+	while(1){ // thuc hien cho den khi left >= right
+		do{
+			left++;
+		}while(A[left] < pivot); // neu A[left] < pivot thi A[left] da o dung vi tri
 		
-		if (left < right) swap(A[left], A[right]);
-		else return right;
+		do{
+			right--;
+		}while(A[right] > pivot); // neu A[right] > pivot thi A[right] da o dung vi tri
+		
+		if (left < right) {
+			swap(A[left], A[right]);
+		}
+		else {
+			return right; 
+		}
 	}
 }
 
@@ -23,18 +33,19 @@ template <class T>
 void quickSort(T A[], int L, int R){
 	if (L < R){
 		int par = partition(A, L, R);
-		quickSort(A, L, par-1);
+		quickSort(A, L, par);
 		quickSort(A, par+1, R);
 	}
 }
 
 int main()
 {
-	int A[5] = {5, 3, 4, 1, 2};
+	int n; cin >> n;
+	int A[n];
+	for (int i=0 ; i<n ; i++) cin >> A[i];
+	quickSort(A, 0, n-1);
 	
-	quickSort(A, 0, 4);
-	
-	for (int x : A) cout << x << " ";
+	for (auto x : A) cout << x << " ";
 	
 	return 0;
 }
